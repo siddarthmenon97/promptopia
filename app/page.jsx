782bc7e9123c3c5fea8d4e41cf08253a8d77c57f
Siddharth <siddharth@Ramyas-MacBook-Pro.local>
2024-10-19 21:00:08 +0530

    created home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,20 @@
+import Feed from '@components/Feed'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        Discover and share
+        <br className='max-md:hidden' />
+        <span className='orange_gradient text-center'>AI-powered prompts</span>
+      </h1>
+      <p className='desc text-center'>
+        Promptopia is a open source ai prompting tool for modern world to discover, 
+        create and share creative prompts
+      </p>
+      <Feed/>
+    </section>
   )
 }
 
